2bfbee440084f750639a76d3dbf340efcf7f02cd
GoodOleJonyCage <adil_ahsan_khan@yahoo.com>
2023-08-31 10:31:47 -0400

    Implemented solution that scrolls the page to the top on every navigate

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,3 +1,28 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1958d76a186e17df675cc0c7f3b9c5a367ac62143da964c0c9952d24ac08a390
-size 1046
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+import { ScrollToTop } from "./Utilities/ScrollToTop"
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+    <BrowserRouter basename={baseUrl}>
+    <ScrollToTop/>
+    <App />
+  </BrowserRouter>);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
