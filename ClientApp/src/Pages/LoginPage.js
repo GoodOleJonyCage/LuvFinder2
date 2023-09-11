04a9a7618daed724517d7d7efa240792040274a2
GoodOleJonyCage <adil_ahsan_khan@yahoo.com>
2023-09-11 09:52:03 -0400

    Restoring back login component

--- ClientApp/src/Pages/LoginPage.js
+++ ClientApp/src/Pages/LoginPage.js
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c0e4af4366030c57fea27e7818b7819be72470ac800fde34f289ac7e027b0e03
-size 108
+import { Login } from '../components/Login'
+
+export const LoginPage = () => {
+
+    return <Login></Login>
+
+}
