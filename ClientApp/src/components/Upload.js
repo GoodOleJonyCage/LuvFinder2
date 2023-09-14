33b032aa703d262c472bc3bcc3b2c1a08e6c4392
GoodOleJonyCage <adil_ahsan_khan@yahoo.com>
2023-09-14 16:04:18 -0400

    Undoing prev change

--- ClientApp/src/components/Upload.js
+++ ClientApp/src/components/Upload.js
@@ -1,3 +1,69 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0e9f626b8212e87024e4b3796884dbb43c130d5a546d4cda6cc3738126c20bc8
-size 2254
+﻿import { useState, useEffect } from 'react'
+import { UploadFile } from '../Services/Services'
+
+export const Upload = (props) => {
+
+    /*const [uploadFile, setUploadFile] = useState();*/
+    const [image, setImage] = useState(null);
+    const [error, seterror] = useState('');
+
+    const submitForm = async (event) => {
+
+        event.preventDefault();
+        const dataArray = new FormData();
+        const filedata = document.getElementById("files").files[0];
+        dataArray.append("files", filedata);
+        dataArray.append('username', props.username)
+
+        try {
+            seterror('');
+            await UploadFile(dataArray);
+            props.setfileuploaded(true);
+        } catch (e) {
+            
+            e.text().then(error =>
+            {
+                //console.log(error);
+                seterror(error);
+            })
+        }
+    };
+
+    const onImageChange = (event) => {
+        if (event.target.files && event.target.files[0]) {
+            setImage(URL.createObjectURL(event.target.files[0]));
+        }
+    }
+    useEffect(() => {
+        if (props.profilePic && props.profilePic.length > 0)
+            setImage(props.profilePic);
+    }, []);
+    
+
+    return <>
+        <form className="form-group upload-container" onSubmit={submitForm} encType="multipart/form-data">
+            <div>
+                <input type="file" id="files" name="files"
+                    onChange={onImageChange} className="filetype file-uploader" />
+            </div>
+            <div>
+                <img alt="" src={image} />
+            </div>
+            <div className="">
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setImage(null);
+                        document.getElementById("files").value = null
+                    }}
+                    className="lab-btn d-block  btn-sm " >Clear Image <i className="icofont-paper-plane"></i></button>
+            </div>
+            <div className="highlight-error">{error}</div>
+            <div className="">
+                <button className="lab-btn d-block upload-btn " type="submit" >Upload<i className="icofont-paper-plane"></i></button>
+            </div>
+        </form>
+    </>
+
+
+}
--- Controllers/UserPicController.cs
+++ Controllers/UserPicController.cs
@@ -1,3 +1,109 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d54868c82a229f371d8acd37a506eede6a73a86f84f70b49359ac396886df150
-size 4454
+﻿using LuvFinder.Helpers;
+using LuvFinder.Models;
+using LuvFinder.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuvFinder.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserPicController : ControllerBase
+    {
+        private readonly LuvFinderContext db;
+        private readonly IConfiguration _config;
+        IWebHostEnvironment _webHostEnvironment;
+        public UserPicController(LuvFinderContext _db, IConfiguration config, IWebHostEnvironment webHostEnvironment)
+        {
+            db = _db;
+            _config = config;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        [HttpPost]
+        [Route("upload")]
+        public ActionResult Upload(List<IFormFile> files)
+        {
+            var username = Request.Form["username"][0];
+            var userID = (new UserController(new LuvFinderContext(), _config)).UserIDByName(username);
+
+            if (files != null)
+            {
+                if (files.Count == 0)
+                    return BadRequest("No Image uploaded");
+
+                try
+                {
+                    //save the file
+                    foreach (var file in files)
+                    {
+                        if (file.Length > 0)
+                        {
+                            if (!file.IsImage())
+                            {
+                                return BadRequest("Has to be an image file");
+                            }
+
+                            # region add image to db
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                var existingEntryDB = db.UserProfilePicsDbs.Where(f => f.UserId == userID).SingleOrDefault();
+                                if (existingEntryDB != null)
+                                    db.UserProfilePicsDbs.Remove(existingEntryDB);
+                                
+                                file.CopyTo(ms);
+                                db.UserProfilePicsDbs.Add(new UserProfilePicsDb()
+                                {
+                                    UserId = userID,
+                                    ImageData = ms.ToArray()
+                                });
+                                db.SaveChanges();
+                                
+                                ms.Close();
+                                ms.Dispose();
+                                //add image to db
+                            }
+                            #endregion 
+
+                            #region save image to file folder
+                            //var fileInfo = new FileInfo(file.FileName);
+                            //var fileNameWithUserID =  userID + fileInfo.Extension;
+                            //var filePath = Helpers.Helpers.GetProfilePicPath(_webHostEnvironment.ContentRootPath,fileNameWithUserID);
+                            ////var filePath = _webHostEnvironment.ContentRootPath
+                            ////    + $"\\ClientApp\\public\\assets\\images\\userprofileimages\\{fileNameWithUserID}";
+
+                            //if (System.IO.File.Exists(filePath))
+                            //    System.IO.File.Delete(filePath);
+
+                            //using (var stream = System.IO.File.Create(filePath))
+                            //{
+                            //    file.CopyTo(stream);
+                            //}
+                            ////save to db 
+
+                            //var existingEntry = db.UserProfilePics.Where(f => f.UserId == userID).SingleOrDefault(); 
+                            //if (existingEntry!= null)
+                            //    db.UserProfilePics.Remove(existingEntry);
+
+                            //db.UserProfilePics.Add(new UserProfilePic()
+                            //{
+                            //    Name = fileNameWithUserID,
+                            //    UserId = userID,
+                            //});
+                            //db.SaveChanges();
+                            #endregion 
+                        }
+                        break;//since we are only uploading one file 
+                    }
+                }
+                catch (Exception exc)
+                {
+                    return BadRequest(exc.Message);
+                }
+                
+            }
+            return Ok("Uploaded");
+        }
+    }
+}
